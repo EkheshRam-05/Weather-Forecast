9037195ce4e203c7532d8f85566d804a503b1482
EkheshRam-05 <71593839+EkheshRam-05@users.noreply.github.com>
2023-01-19 17:11:10 +0530

    Update weather.js

--- weather.js
+++ weather.js
@@ -4,7 +4,7 @@ var city = "";
 var flag = 0;
 
 function findCoords() {
-	console.log(city, "helo ");
+// 	console.log(city, "helo ");
 	// document.getElementById('wait').style.display = "block";
 	if (flag) {
 		if (window.innerHeight < 500 || window.innerWidth < 500) {
@@ -13,12 +13,12 @@ function findCoords() {
 			city = document.getElementById("alt-city").value;
 		}
 	}
-	console.log(city);
+// 	console.log(city);
 	fetch("https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=fee9a6420fc2791c96cc952b75088ff8&units=metric")
 	.then(response =>{
 		return response.json();
 	}).then(data =>{
-		console.log(data);
+// 		console.log(data);
 		console.log(data.main.feels_like, data.main.humidity, data.main.pressure, data.main.temp, data.visibility, data.weather[0].description, data.weather[0].main, data.wind.deg, data.wind.speed);
 		document.getElementById('feels_like').textContent = data.main.feels_like + "°C";
 		document.getElementById('temp').textContent = data.main.temp + "°C";
@@ -34,7 +34,7 @@ function findCoords() {
 
 function findCoords_load(){
 	city = city_temp;
-	console.log(city, city_temp);
+// 	console.log(city, city_temp);
 	findCoords();
 	flag = 1;
 }
